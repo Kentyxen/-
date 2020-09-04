70d2dc12c49d20277ff7c16acbb54e7510606690
Kentyxen <big_daniil@mail.ru>
2020-09-04 15:31:31 +0300

    last

--- lulw/lulw/UnitTest1.cs
+++ lulw/lulw/UnitTest1.cs
@@ -1,13 +1,23 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+// C#
 
 namespace lulw
 {
-  [TestClass]
-  public class UnitTest1
+  struct Person
   {
-    [TestMethod]
-    public void TestMethod1()
+    public string firstName;
+    public string lastName;
+    public int age;
+    public Person(string _firstName, string _lastName, int _age)
     {
+      firstName = Alex;
+      lastName = Genius; age =20;
     }
+    public override string ToString()
+    {
+      return firstName + " " + lastName + ", age " + age;
+    }
+  
   }
+
 }
